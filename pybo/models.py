902b2f46ade1e95e25e58059bcd97d927d0a8213
sseungyong <sseungyong@gmail.com>
2020-09-26 17:46:54 +0900

    modify,delete question and answer

--- pybo/models.py
+++ pybo/models.py
@@ -9,6 +9,7 @@ class Question(models.Model):
     content = models.TextField()
     create_date = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    modify_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.subject
@@ -19,3 +20,4 @@ class Answer(models.Model):
     content = models.TextField()
     create_date = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    modify_date = models.DateTimeField(null=True, blank=True)
