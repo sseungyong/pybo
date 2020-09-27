083a550e97467204b0c248c093739099e18e41d7
sseungyong <sseungyong@gmail.com>
2020-09-28 01:17:44 +0900

    searching and sorting

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
+from django.db.models import Q, Count
 
 from ..models import Question
 
@@ -10,15 +11,33 @@ def index(request):
     """
     # 입력 param
     page = request.GET.get('page', '1')  # page
+    kw = request.GET.get('kw', '')  # search text
+    so = request.GET.get('so', 'recent')  # sorting
 
     # 조회
-    question_list = Question.objects.order_by('-create_date')
+    # 정렬
+    if so == 'recommend':
+        question_list = Question.objects.annotate(
+            num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    elif so == 'popular':
+        question_list = Question.objects.annotate(num_answer=Count(
+            'answer')).order_by('-num_answer', '-create_date')
+    else:  # recent
+        question_list = Question.objects.order_by('-create_date')
+
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  # 제목검색
+            Q(content__icontains=kw) |  # 내용검색
+            Q(author__username__icontains=kw) |  # 질문 글쓴이검색
+            Q(answer__author__username__icontains=kw)  # 답변 글쓴이검색
+        ).distinct()
 
     # paging
     paginator = Paginator(question_list, 10)
     page_obj = paginator.get_page(page)
 
-    context = {'question_list': page_obj}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw, 'so': so}
     return render(request, 'pybo/question_list.html', context)
 
 
