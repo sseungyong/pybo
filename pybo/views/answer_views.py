b0fd46a76dbc9a633c850294bef203d0045718cc
sseungyong <sseungyong@gmail.com>
2020-09-28 00:04:06 +0900

    anchor and markdown

--- pybo/views/answer_views.py
+++ pybo/views/answer_views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, resolve_url
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
@@ -21,7 +21,7 @@ def answer_create(request, question_id):
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
-            return redirect('pybo:detail', question_id=question.id)
+            return redirect("{}#answer_{}".format(resolve_url('pybo:detail', question_id=question.id), answer.id))
     else:
         form = AnswerForm()
     context = {'question': question, 'form': form}
@@ -36,7 +36,7 @@ def answer_modify(request, answer_id):
     answer = get_object_or_404(Answer, pk=answer_id)
     if request.user != answer.author:
         messages.error(request, '수정권한이 없습니다.')
-        return redirect('pybo:detail', question_id=answer.question.id)
+        return redirect("{}#answer_{}".format(resolve_url('pybo:detail', question_id=answer.question.id), answer.id))
 
     if request.method == 'POST':
         form = AnswerForm(request.POST, instance=answer)
@@ -45,7 +45,7 @@ def answer_modify(request, answer_id):
             answer.author = request.user
             answer.modify_date = timezone.now()
             answer.save()
-            return redirect('pybo:detail', question_id=answer.question.id)
+            return redirect("{}#answer_{}".format(resolve_url('pybo:detail', question_id=answer.question.id), answer.id))
     else:
         form = AnswerForm(instance=answer)
     context = {'answer': answer, 'form': form}
